f2eacea5578db4de4df811302e32f431a1f2ab8d
AZffdr <152608992+AZffdr@users.noreply.github.com>
2024-03-14 14:45:42 +0800

    app.js

--- app.js
+++ app.js
@@ -8,7 +8,7 @@ const loginApi = qs.loginApi
 
 // 签到并提交每日体温报告 
 login().then((token) => {
-  setTimeout(sign(token),3000)
+  setTimeout("sign(token)",3000)
   // sign(token)
   //studentReportInfo(token)
 })
